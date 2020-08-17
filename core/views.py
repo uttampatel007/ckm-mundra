1aae7ce07721f6d358a2a7396b8a202db2c7bdd4
Uttam Patel <uttam11.velani11@gmail.com>
2020-08-17 11:09:14 +0530

    all-ready-commit

--- core/views.py
+++ core/views.py
@@ -1,30 +1,45 @@
 from django.shortcuts import render
+from . models import Contact
 
-def home(requests):
-	return render(requests,'core/index.html')
+def home(request):
+	return render(request,'core/index.html')
 
-def admission(requests):
-	return render(requests,'core/admission.html')
+def admission(request):
+	return render(request,'core/admission.html')
 
-def gallery(requests):
-	return render(requests, 'core/gallery.html')
+def gallery(request):
+	return render(request, 'core/gallery.html')
 
-def result(requests):
-	return render(requests, 'core/results.html')
+def result(request):
+	return render(request, 'core/results.html')
 
-def contact(requests):
-	return render(requests, 'core/contact.html')
+def contact(request):
+	context = {}
+	if request.method == "POST":
+		name = request.POST.get('author')
+		email = request.POST.get('email')
+		subject = request.POST.get('subject')
+		message = request.POST.get('comment')
+
+		c = Contact(name=name,email=email,subject=subject,message=message)
+		c.save()
+
+		submit_message = " આભાર. અમે જલ્દી જ તમારો સંપર્ક કરીશું."
+		context = {'submit_message':submit_message}
+		return render(request, 'core/contact.html',context)
+	else:
+		return render(request, 'core/contact.html')
 # 
 
-def history(requests):
-	return render(requests, 'core/about.html')
+def history(request):
+	return render(request, 'core/about.html')
 # 
 
-def school_family(requests):
-	return render(requests, 'core/schoolFamily.html')
+def school_family(request):
+	return render(request, 'core/schoolFamily.html')
 
-def memories(requests):
-	return render(requests, 'core/memories.html')
+def memories(request):
+	return render(request, 'core/memories.html')
 
-def co_curricular(requests):
+def co_curricular(request):
 	return render(request, 'core/activity.html')
